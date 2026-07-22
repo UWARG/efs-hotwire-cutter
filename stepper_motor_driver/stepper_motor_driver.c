c009bf9f49563b9f4de97ec5c70a8a75b14f40ef
chaosium43 <pyrokek420@gmail.com>
2026-07-22 19:12:36 -0400

    added sg_result reading code

--- stepper_motor_driver/stepper_motor_driver.c
+++ stepper_motor_driver/stepper_motor_driver.c
@@ -27,6 +27,12 @@
 #define MICROSTEPS          8
 #define STEPS_PER_REV       (MOTOR_STEPS_PER_REV * MICROSTEPS) // 3200 steps
 
+#define SGTHRS_VALUE 128 // idk what this does for SGTHRS we're just going to use the middle value for now because that seems the most correct XD
+#define SG_RESULT_THRESHOLD 67 // any SG_RESULT value lower than this will be considered a stall
+
+#define SGTHRS 0x40 // address of the stall guard threshold register, higher = more sensitive detection of stalling, min 0 max 255
+#define SG_RESULT 0x41 // StallGuard4 generates a value, goes from 0 to 510 with 0 being highest load and 510 being lowest load
+
 const uint32_t FIXED_SPEED_DELAY_US = 1000;
 //set to match motor
 const float X_MM_PER_REV = 1.5875f; // 1.5875mm per revolution for the lead screw
@@ -48,8 +54,23 @@ uint8_t calculateCRC(uint8_t* datagram, uint8_t len) {
     return crc;
 }
 
-void writeTMCRegister(uint8_t target_address, uint8_t reg, uint32_t data) {
-    uint8_t packet[8];
+uint32_t readTMCRegister(uint8_t target_address, uint8_t reg) {
+    uint8_t packet[4];
+    uint32_t result = 0;
+
+    packet[0] = 0x05;         // Sync bits
+    packet[1] = target_address;         // X-axis node address
+    packet[2] = reg | 0x80;   // Add write flag bit
+    packet[3] = calculateCRC(packet, 4);
+    
+    uart_write_blocking(VM_UART, packet, sizeof(packet)); // send read request
+    uart_read_blocking(VM_UART, (uint8_t*)(&result), sizeof(result));
+    return result;
+}
+
+void writeTMCRegister(uint8_t target_address, uint8_t reg, uint32_t data)
+{
+    uint8_t packet[4];
     packet[0] = 0x05;         // Sync bits
     packet[1] = target_address;         // X-axis node address
     packet[2] = reg | 0x80;   // Add write flag bit
@@ -60,9 +81,6 @@ void writeTMCRegister(uint8_t target_address, uint8_t reg, uint32_t data) {
     packet[6] = data & 0xFF;
     
     packet[7] = calculateCRC(packet, 7);
-    
-    // Direct hardware write via Pico SDK tracking
-    uart_write_blocking(VM_UART, packet, 8);
 }
 
 
@@ -81,6 +99,7 @@ void move_X(float distance_mm){
         sleep_us(FIXED_SPEED_DELAY_US);
     }
 }
+
 int main()
 {
     stdio_init_all();
@@ -108,6 +127,7 @@ int main()
     // 4. Send safety configuration registers to the X-Axis Driver (Node Address 0)
     writeTMCRegister(X_UART_ADDR, 0x10, 0x00001F10); // Sets conservative, safe operating currents
     writeTMCRegister(X_UART_ADDR, 0x6C, 0x04000053); // Configures smooth 16 microsteps (dedge = 0)
+    writeTMCRegister(X_UART_ADDR, SGTHRS, SGTHRS_VALUE);
 
     sleep_ms(1000); // Wait 1 second before beginning test routine
     // 5. Main Test Loop
@@ -124,4 +144,9 @@ int main()
     // Stop completely for 1.5 seconds before repeating
     sleep_ms(1500);
 
+    while (true)
+    {
+        printf("SG_RESULT: %d\n", readTMCRegister(X_UART_ADDR, SG_RESULT));
+        sleep_ms(1000);
+    }
 }
